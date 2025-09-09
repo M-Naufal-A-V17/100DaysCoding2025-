074d482a7ed3b4984345c0f2638252564a84a9f4
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-09 17:29:57 +0800

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,17 @@
+public class day2 {
+    public static void main(String[] args) {
+        //penggunaan println()
+        System.out.println("m naufal");
+        System.out.println("al varzy");
+        //penggunaan print()
+        System.out.print("m naufal");
+        System.out.print("al varzy");
+        //penggunaan printf
+        String nama = "naufal";
+        int umur = 18; 
+        System.out.printf("Nama: %s\n",nama);
+        System.out.printf("umur: %dtahun\n" ,umur);
+
+
+    }
+} 

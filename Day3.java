06b9eccda9a03f99498329e2d490268d113e5a52
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-10 17:15:26 +0800

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,11 @@
+/**
+ * program: komentar dokumentasi
+ * Deskripsi: penggunaan komentar dokumentasi
+ */
+public class Day3{
+    public static void main (String[] args) {
+        System.out.println("vall");
+        /* komentar multi-baris penjelasan panjang*/
+        // komentar satu baris penjelasan singkat
+    }
+}

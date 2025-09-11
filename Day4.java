939dee54e5611ef7b581043ca9812c2bc675ac69
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-11 19:55:55 +0800

    Add files via upload

---
+++ Day4.java
@@ -0,0 +1,18 @@
+public class Day4 {
+    public static void main(String[] args) {
+        // \n untuk baris baru
+       System.out.println("hai vall \nthank you") ;
+       // \t untuk tab
+       System.out.println("nama: \tvall");
+       // \" untuk tanda kutip
+       System.out.println("\"halo\"");
+       // \' tanda kutip tunggal
+       System.out.println("\'nice vall\'");
+       // \menampilkan karakter backslash
+       System.out.println("\\hello");
+       // \r carriage return
+       System.out.println("hello \rworld");
+       // \b backspace
+       System.out.println("hello\b!");
+    }
+}

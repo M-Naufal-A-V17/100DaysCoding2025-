0b6e9a2f4b919c1bc7dd2ebe1696995dc4b9df75
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-12 16:21:19 +0800

    Add files via upload

---
+++ Day5.java
@@ -0,0 +1,18 @@
+public class Day5{
+    public static void main(String[] args){
+        // tipe data byte
+       byte umur = 18;
+       System.out.println("umur (byte): " + umur);
+       // tipe data int
+       int jumlahpenduduk  = 1800000000;
+       System.out.println("jumlah penduduk sebuah negara (int): " + jumlahpenduduk);
+       // tipe data long
+       long jarakpulau = 9480730472580800L; 
+       System.out.println("jarak pulau A ke pulau B (long): " + jarakpulau);
+       // tipe data short
+       short jumlahsebuahbangunan = 513;
+       System.out.println("jumlah bangunan di sebuah kota (short): " + jumlahsebuahbangunan);
+
+
+    }
+}

0e3d7dadaf6432a34d1fe8e8a0917af9998bc084
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-14 19:14:01 +0800

    Add files via upload

---
+++ Day7.java
@@ -0,0 +1,13 @@
+public class Day7{
+    public static void main(String[] args) {
+        // Tipe data char dan boolean
+
+         char nilai = 'A';
+         boolean lulus = true;
+
+         System.out.println("Nilai : " + nilai);
+         System.out.println("Apakah lulus?" + lulus);
+      
+
+    }
+}

cf23ebd28ef40d7994f5e16e78c6e93ac5fd2af2
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-15 20:21:12 +0800

    Add files via upload

---
+++ Day8.java
@@ -0,0 +1,14 @@
+public class Day8{
+    public static void main(String[] args) {
+        // Tipe data string untuk teks
+
+        String buah1 = "apel";
+        String buah2 = "mangga";
+        String buah3 = "pisang";
+
+        System.out.println("Nama buah 1: " + buah1);
+        System.out.println("Nama buah 2: " + buah2);
+        System.out.println("Nama buah 3: " + buah3);
+    }
+
+}

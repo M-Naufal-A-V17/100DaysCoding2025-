a47c7d0eda04296d8f6c2b75f1a989559acbe3da
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-16 19:27:33 +0800

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,23 @@
+public class Day9{
+    public static void main(String[] args) {
+        // Deklarasi
+        String buah;
+        int jumlah;
+
+        // Inisialisasi
+        buah = "Jeruk";
+        jumlah = 5;
+
+        System.out.println("Buah awal : " + buah);
+        System.out.println("Jumlah awal : " + jumlah);
+
+        // Update variabel
+        buah = "Anggur";
+        jumlah = 12;
+
+        System.out.println("Buah update : " + buah);
+        System.out.println("Jumlah update : " + jumlah);
+         
+
+    }
+}

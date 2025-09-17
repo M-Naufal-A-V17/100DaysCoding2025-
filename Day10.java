ccea3a07ddfe2984b4d0c057d41cd7524ca61b63
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-17 20:01:25 +0800

    Add files via upload

---
+++ Day10.java
@@ -0,0 +1,15 @@
+public class  Day10{
+    public static void main(String[] args) {
+        // Final variabel
+
+        final int tahun = 2025;
+        final String negara = "indonesia";
+        final double pi = 3.14;
+
+         System.out.println("Tahun sekarang: " + tahun);
+         System.out.println("Negara: " + negara);
+         System.out.println("Nilai Pi: " + pi);
+
+    }
+
+}

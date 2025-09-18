b8808a39f849108d6788530cdb09d3ec4cb10b4c
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-18 20:37:39 +0800

    Add files via upload

---
+++ Day11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Day11{
+    public static void main(String[] args) {
+        
+        Scanner input = new 
+        Scanner(System.in);
+
+        System.out.println("Masukkan nama:");
+        String nama = input.nextLine();
+        System.out.println("Masukkan umur:");
+        int umur = input.nextInt();
+        System.out.println("Halo" + nama + ", umur kamu " + umur + " tahun.");
+        
+      
+    }
+}

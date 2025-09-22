e0ac9d0e85a728286b57568ae5ac4835cf448710
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-22 22:48:02 +0800

    Add files via upload

---
+++ Day15.java
@@ -0,0 +1,19 @@
+public class Day15 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int a = 10;
+        int b = 4;
+
+        //operasi aritmatika
+        int tambah = a + b;
+        int kurang = a - b;
+
+        //Output hasil
+        System.out.println(" Nilai a = " + a);
+        System.out.println(" Nilai b = " + b);
+        System.out.println(" Hasil penjumlahan (a + b) " + tambah);
+        System.out.println(" Hasil pengurangan (a - b) " + kurang);
+        
+    }
+    
+}

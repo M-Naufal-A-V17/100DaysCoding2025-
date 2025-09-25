07a984676b2420618e0abfbb597ee705685ad28d
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-25 22:11:36 +0800

    Add files via upload

---
+++ Day18.java
@@ -0,0 +1,16 @@
+public class Day18 {
+    public static void main(String[] args) {
+        int  angka = 10;
+        double hasilDouble = angka;
+        System.out.println("Nilai int: " + angka);
+        System.out.println("hasil konversi otomatis ke double: " + hasilDouble);
+
+        double nilai = 9.7;
+        int hasilint = (int) nilai;
+        System.out.println("nilai double: " + nilai);
+        System.out.println("Hasil konversi manual ke int: " + hasilint);
+
+
+    }
+    
+}

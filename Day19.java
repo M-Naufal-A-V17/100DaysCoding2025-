8fc8b3752c4b816c2a4f34567999bd4f14a52364
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-26 21:50:45 +0800

    Add files via upload

---
+++ Day19.java
@@ -0,0 +1,12 @@
+public class Day19 {
+    public static void main(String[] args) {
+        
+        double nilaiDesimal = 12.75;
+        int nilaiBulat = (int) nilaiDesimal;
+
+        System.out.println("Nilai double: " + nilaiDesimal);
+        System.out.println("Niali int setelah konversi: " + nilaiBulat);
+
+    }
+    
+}

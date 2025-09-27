e808be0b6aacc230d4c4adb4046ccc9cc5df1704
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-27 22:05:11 +0800

    Add files via upload

---
+++ Day20.java
@@ -0,0 +1,17 @@
+public class Day20 {
+    public static void main(String[] args) {
+        int angka = 100;
+        double desimal = 12.5;
+        boolean status = true;
+
+        String strANGKA = String.valueOf(angka);
+        String strDESIMAL = String.valueOf(desimal);
+        String strSTATUS= String.valueOf(status);
+
+        System.out.println("Angka dalam string: " + strANGKA);
+        System.out.println("Desimal dalam string: " + strDESIMAL);
+        System.out.println("Status dalam string: " + strSTATUS);
+
+    }
+    
+}

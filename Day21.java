b8895d9e4b38f50dcfc46eb8a84a6c5bcb377cdd
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-28 22:56:10 +0800

    Add files via upload

---
+++ Day21.java
@@ -0,0 +1,15 @@
+public class Day21 {
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 10;
+
+        System.out.println("Sebelum ditukar: a = " + a + ", b = " + b);
+
+        a = a + b ;
+        b = a - b ;
+        a = a - b;
+
+        System.out.println("Sesudah ditukar: a = " + a + ", b = " + b);
+    }
+}

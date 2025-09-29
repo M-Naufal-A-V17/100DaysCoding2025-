55c1038ead48a66089bc6b41f3ed8267647262f0
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-09-29 22:06:35 +0800

    Add files via upload

---
+++ Day22.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Day22 {
+    public static void main(String[] args) {
+
+         Scanner input = new Scanner(System.in);
+
+         System.out.print("Masukkan sisi persegi: ");
+         int sisi = input.nextInt();
+
+         int luas = sisi * sisi;
+
+         System.out.println("Luas Persegi = " + luas);
+    }
+    
+}

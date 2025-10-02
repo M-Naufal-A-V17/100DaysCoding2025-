7705a08b393917e13971dd5ba952c886ca64a6a1
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-02 21:34:47 +0800

    Add files via upload

---
+++ Day25.java
@@ -0,0 +1,17 @@
+public class Day25 {
+    public static void main(String[] args) {
+        
+        int x = 5;
+
+        System.out.println("Nilai awal x = " + x);
+
+        //increment
+        x = x + 1;
+        System.out.println("Setelah increment, nilai x = " + x);
+
+        //Decrement
+        x = x - 1;
+        System.out.println("Setelah decrement, nilai x = " + x);
+    }
+    
+}

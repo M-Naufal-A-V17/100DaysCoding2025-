3fd143b0cb54c9e0c0ed37e9d75366d5a56b4a97
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-03 21:25:49 +0800

    Add files via upload

---
+++ Day26.java
@@ -0,0 +1,22 @@
+public class Day26 {
+    public static void main(String[] args) {
+        int x = 10; // penugasan biasa
+        System.out.println("Nilai awal x = " + x);
+
+        x += 5; // sama dengan x = x + 5
+        System.out.println("Setelah x += 5, nilai x = " + x);
+
+        x -= 3; // sama dengan x = x - 3
+        System.out.println("Setelah x -= 3, nilai x = " + x);
+
+        x *= 2; // sama dengan x = x * 2
+        System.out.println("Setelah x *= 2, nilai x = " + x);
+
+        x /= 4; // sama dengan x = x / 4
+        System.out.println("Setelah x /= 4, nilai x = " + x);
+
+        x %= 3; // sama dengan x = x % 3
+        System.out.println("Setelah x %= 3, nilai x = " + x);
+    }
+    
+}

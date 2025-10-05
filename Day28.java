e730660b328e56b24b28f532d1944e3a1ebf76c8
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-05 21:37:13 +0800

    Add files via upload

---
+++ Day28.java
@@ -0,0 +1,12 @@
+public class Day28 {
+    public static void main(String[] args) {
+          int a = 5;
+        int b = 7;
+
+        System.out.println("Apakah a sama dengan b? " + (a == b));
+        System.out.println("Apakah a tidak sama dengan b? " + (a != b));
+
+
+    }
+    
+}

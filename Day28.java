5c83c3e060dd15ac76085c79f101938b27de6bc4
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-05 21:37:42 +0800

    Update Day28.java

--- Day28.java
+++ Day28.java
@@ -1,6 +1,6 @@
 public class Day28 {
     public static void main(String[] args) {
-          int a = 5;
+        int a = 5;
         int b = 7;
 
         System.out.println("Apakah a sama dengan b? " + (a == b));
@@ -10,3 +10,4 @@ public class Day28 {
     }
     
 }
+

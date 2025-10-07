ccb1e64e4cd3b4fb9be7931b4b4c94c5692318d6
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-07 22:07:44 +0800

    Add files via upload

---
+++ Day30.java
@@ -0,0 +1,10 @@
+public class Day30 {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 8;
+
+        System.out.println("Apakah a lebih besar atau sama dengan b? " + (a >= b));
+        System.out.println("Apakah a lebih kecil atau sama dengan b? " + (a <= b));
+    }
+    
+}

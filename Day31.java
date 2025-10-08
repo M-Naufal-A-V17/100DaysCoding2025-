3a34d0104d686903985de0600132f3620753cb0a
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-08 22:12:24 +0800

    Add files via upload

---
+++ Day31.java
@@ -0,0 +1,11 @@
+public class Day31 {
+    public static void main(String[] args) {
+        int umur = 20;
+        boolean punyaSIM = true;
+
+        boolean bolehMengemudi = (umur >= 18) && punyaSIM;
+
+        System.out.println("Apakah boleh mengemudi? " + bolehMengemudi);
+    }
+    
+}

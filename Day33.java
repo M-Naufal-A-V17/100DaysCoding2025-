60f884505a3118145e64bf30cf108971f7af1678
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-10 20:08:25 +0800

    Add files via upload

---
+++ Day33.java
@@ -0,0 +1,10 @@
+public class Day33 {
+    public static void main(String[] args) {
+         boolean hujan = true;
+
+        boolean bolehKeluar = !hujan;
+
+        System.out.println("Apakah boleh keluar rumah? " + bolehKeluar);
+    }
+    
+}

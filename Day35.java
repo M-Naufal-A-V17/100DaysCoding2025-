36981369ea3d05df2cb2b52bee9eb3f0fbe43190
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-12 22:50:19 +0800

    Add files via upload

---
+++ Day35.java
@@ -0,0 +1,11 @@
+public class Day35 {
+    public static void main(String[] args) {
+        int nilai = 80;
+
+        if (nilai >= 75) {
+            System.out.println("Selamat, Anda lulus!");
+    }
+    
+  }
+}
+ 

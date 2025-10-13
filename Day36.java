ef877b2d9e7bcb3f5535b3667a14e966e4f298fd
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-13 19:36:07 +0800

    Add files via upload

---
+++ Day36.java
@@ -0,0 +1,19 @@
+public class Day36 {
+    public static void main(String[] args) {
+
+        int nilai = 85;
+        
+          if (nilai >= 75) {
+            System.out.println("Selamat, Anda lulus!");
+            
+            if (nilai >= 90) {
+                System.out.println("Nilai Anda sangat baik!");
+            } else {
+                System.out.println("Nilai Anda baik, tapi bisa lebih bagus lagi!");
+            }
+        } else {
+            System.out.println("Maaf, Anda belum lulus.");
+ }
+    }
+    
+}

f32b8622df0840634a6c53ddfd96c4c6c1afe500
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-14 22:41:10 +0800

    Add files via upload

---
+++ Day37.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Day37 {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka " + angka + " adalah bilangan genap.");
+        } else if (angka % 2 != 0) {
+            System.out.println("Angka " + angka + " adalah bilangan ganjil.");
+       }
+    }
+    
+}

917af545fefad9b5eb0570cc0d627ffa68130cd6
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-26 21:02:57 +0800

    Add files via upload

---
+++ Day49.java
@@ -0,0 +1,11 @@
+public class Day49 {
+    public static void main(String[] args) {
+        int angka = 10;
+        String hasil;
+
+        hasil = (angka % 2 == 0) ? "Angka Genap" : "Angka Ganjil";
+
+        System.out.println("Hasil: " + hasil);
+    }
+    
+}

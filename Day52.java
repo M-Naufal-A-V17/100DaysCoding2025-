38911152918c871ce8b6cdeef289749da6a76081
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-29 21:52:19 +0800

    Add files via upload

---
+++ Day52.java
@@ -0,0 +1,12 @@
+public class Day52 {
+    public static void main(String[] args) {
+        
+         int angka = 1;
+
+        while (angka <= 5) {
+            System.out.println("Angka: " + angka);
+            angka++;
+        }
+    }
+    
+}

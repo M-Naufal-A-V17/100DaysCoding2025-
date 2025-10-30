900b66feb7b583225e3f1ca76275b44f5bbdb46c
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-10-30 23:29:38 +0800

    Add files via upload

---
+++ Day53.java
@@ -0,0 +1,22 @@
+public class Day53 {
+    public static void main(String[] args) {
+        
+          int angka = 1;
+
+        while (angka <= 10) {
+            System.out.println("Angka: " + angka);
+
+            if (angka == 5) {
+                break;   // menghentikan perulangan
+            }
+
+            angka++;
+        }
+
+        System.out.println("Perulangan berhenti.");
+        }
+    
+
+    }
+    
+

79fb6345d14c48d088baf996d32b3c25191a0536
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-11-02 22:22:05 +0800

    Add files via upload

---
+++ Day56.java
@@ -0,0 +1,15 @@
+public class Day56 {
+    public static void main(String[] args) {
+        
+         int angka = 1;
+
+        do {
+            System.out.println("Angka: " + angka);
+            angka = angka + 1;
+        }
+        while (angka <= 5);
+
+        System.out.println("Selesai!");
+    }
+    
+}

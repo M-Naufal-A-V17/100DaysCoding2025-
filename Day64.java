cc24e4a671938648edd469d78debe917bac449f5
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-11-10 21:54:05 +0800

    Add files via upload

---
+++ Day64.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Day64 {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai M: ");
+        int m = input.nextInt();
+
+        System.out.print("Masukkan nilai N: ");
+        int n = input.nextInt();
+
+        int hasil = 1;
+
+        for(int i = 1; i <= n; i++){
+            hasil = hasil * m;
+        }
+
+        System.out.println("Hasil " + m + " pangkat " + n + " adalah: " + hasil);
+    }
+    
+}

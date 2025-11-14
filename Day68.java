b17ff87758e8992a59a7d9e4a8d9c83bfb801b30
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-11-14 19:31:34 +0800

    Add files via upload

---
+++ Day68.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Day68 {
+    public static void main(String[] args) {
+        
+         Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah bintang: ");
+        int n = input.nextInt();
+
+        for(int i = 0; i <= n; i++){
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+                
+            }
+            System.out.print("\n");
+        }
+    }
+    
+}

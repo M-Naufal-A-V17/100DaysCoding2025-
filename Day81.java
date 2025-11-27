896bc599395851f71dd3dcde6a7b6fa0eff33932
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-11-27 23:13:42 +0800

    Add files via upload

---
+++ Day81.java
@@ -0,0 +1,13 @@
+public class Day81 {
+    public static void main(String[] args) {
+       
+        int[] angka = {10, 20, 30, 40, 50};
+
+       
+        System.out.println("Elemen pertama : " + angka[0]);
+        System.out.println("Elemen ketiga  : " + angka[2]);
+        System.out.println("Elemen terakhir: " + angka[4]);
+    }
+
+    
+}

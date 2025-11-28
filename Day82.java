06e7fe5926c0c5465c9990f3c39d96a5b2676530
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-11-28 22:34:28 +0800

    Add files via upload

---
+++ Day82.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Day82 {
+    public static void main(String[] args) {
+        
+         Scanner input = new Scanner(System.in);
+
+       
+        int[] angka = new int[5];
+
+        
+        for (int i = 0; i < angka.length; i++) {
+            System.out.print("Masukkan angka ke-" + (i + 1) + ": ");
+            angka[i] = input.nextInt();
+        }
+
+        System.out.println("\n=== Semua Elemen Array ===");
+
+       
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Index " + i + " = " + angka[i]);
+        }
+    }
+    
+}

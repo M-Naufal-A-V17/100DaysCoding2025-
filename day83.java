d8644c5a881d2d449d357472388c43a937a34e2d
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-11-29 22:45:17 +0800

    Add files via upload

---
+++ day83.java
@@ -0,0 +1,15 @@
+public class day83 {
+    public static void main(String[] args) {
+       int[] angka = {5, 10, 15, 20, 25};
+
+        int total = 0;
+
+       
+        for (int i = 0; i < angka.length; i++) {
+            total = total + angka[i];
+        }
+
+        System.out.println("Total jumlah elemen array = " +total);
+    }
+    
+}

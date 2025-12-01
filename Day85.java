8410507e1e43dfae0a12c53f16bb1b155361ae33
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-12-01 21:27:14 +0800

    Add files via upload

---
+++ Day85.java
@@ -0,0 +1,16 @@
+public class Day85{
+    public static void main(String[] args) {
+        
+        int[] angka = {12, 7, 22, 5, 30, 18};
+
+        int maks = angka[0];   
+
+        for (int i = 1; i < angka.length; i++) {
+            if (angka[i] > maks) {
+                maks = angka[i];
+            }
+        }
+
+        System.out.println("Angka maksimal dalam array adalah: " + maks);
+    }
+}

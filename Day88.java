756fe3dcb76082f640b34e8aa12303a3371a16d9
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-12-04 23:06:48 +0800

    Add files via upload

---
+++ Day88.java
@@ -0,0 +1,13 @@
+public class Day88 {
+    public static void main(String[] args) {
+        
+         int[] angka = {5, 10, 15, 20, 25};
+
+        
+
+        for (int nilai : angka) {
+            System.out.println("Elemen: " + nilai);
+        }
+    }
+    
+}

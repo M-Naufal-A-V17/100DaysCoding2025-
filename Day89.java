29be0539f49d7ceabc5ef4bbc5fcdc9a9eee9f86
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-12-05 16:31:14 +0800

    Add files via upload

---
+++ Day89.java
@@ -0,0 +1,13 @@
+public class Day89 {
+    // Method void
+    static void sapaPengguna() {
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+
+        // Memanggil method
+        sapaPengguna();
+    
+   }
+}

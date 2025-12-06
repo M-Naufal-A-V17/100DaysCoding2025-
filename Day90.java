760f631cc0ab5b2ec0dd42ed05bc12419754f215
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-12-06 09:57:53 +0800

    Add files via upload

---
+++ Day90.java
@@ -0,0 +1,14 @@
+public class Day90 {
+    
+     static int tambah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int hasil = tambah(5, 7); 
+
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+    
+}

7cf631261f49c33660ea0b4fc30527227db8a846
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-12-07 19:43:24 +0800

    Add files via upload

---
+++ Day91.java
@@ -0,0 +1,14 @@
+public class Day91 {
+    
+ 
+    static void sapa(String nama) {
+        System.out.println("Halo, " + nama + "!");
+    }
+
+    public static void main(String[] args) {
+
+        sapa("Rizky");
+        sapa("Andi");
+    
+    }
+}

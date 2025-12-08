b1e3bdef545ce3f9c587e6443bff8f498f2bd328
M-Naufal-A-V17 <vall1700n@gmail.com>
2025-12-08 20:38:07 +0800

    Add files via upload

---
+++ Day92.java
@@ -0,0 +1,14 @@
+public class Day92 {
+      
+    static int kaliDua(int angka) {
+        return angka * 2;
+    }
+
+    public static void main(String[] args) {
+
+        int hasil = kaliDua(5);
+
+        System.out.println("Hasilnya adalah: " + hasil);
+    
+    }
+}
